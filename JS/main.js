6261672266341be35bec402c79729eff52e9a497
NguyenDangTo <92070209+NguyenDangTo@users.noreply.github.com>
2021-11-01 14:26:22 +0700

    Raw

---
+++ JS/main.js
@@ -0,0 +1,45 @@
+/* --------------Open in Browser------------- */
+
+const openBtn = document.getElementById("openBtn"),
+      formContainer = document.getElementsByClassName("formContainer"),
+      btnContainer = document.getElementsByClassName("btnContainer");
+
+
+openBtn.addEventListener("click", enterUsername)
+
+function enterUsername() {
+    btnContainer[1].style.display = "none";
+    formContainer[0].style.display = "block";
+}
+
+/* ---------------Open&Close Nav Mobile Menu-------------- */
+
+const 
+    openMobileMenuBtn = document.querySelector("#menuBtn"),
+    closeMobileMenuBtn = document.querySelector("#closeNavMobile"),
+    backdrop = document.querySelector(".backdrop"),
+    mobileMenu = document.querySelector(".navMobileContainer");
+
+openMobileMenuBtn.addEventListener("click", openMobileMenu)
+closeMobileMenuBtn.addEventListener("click", closeMobileMenu)
+backdrop.addEventListener("click",closeMobileMenu)
+mobileMenu.addEventListener("click", function(ev) {
+    ev.stopPropagation()
+})
+
+
+
+
+function openMobileMenu() {
+    backdrop.classList.add("open")
+}
+
+function closeMobileMenu() {
+    backdrop.classList.remove("open")
+}
+
+
+
+
+
+
